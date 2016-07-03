c2ca242b15b2697158e09940e267a71b6f5eb66d
Hollovv <darkhollovv@gmail.com>
2016-07-03 23:10:33 +0600

    Test1.0

--- function.php
+++ function.php
@@ -1,53 +1,40 @@
 <?php 
-function MyExpl(array $separator, string $string):array
+function MyExpl(array $separator, string $string)
 {
 	$arr = str_split($string);
 	$arr_length = count($arr);
 	
 	$sep_length = count($separator);
+	
+	$result = '';
 	
 	if($separator == "" ||
 	   $separator == false ||
 	   $separator == null) 
 		return false;
 	
-	for($i=0; $i<$arr_length; $i++){
+	for($i=0; $i<$arr_length; $i++)
 		for($j=0; $j<$sep_length; $j++)
-			if($arr[$i] == $sep[$j])
-				$arr[$i] = " ";
-	
-		echo $arr[$i];
-	}
-		/*else
-				echo $arr[$i];
-		/*foreach($separator as $delimiter){
-                echo "$value ";
-                if($value == $delimiter){
-                    $value = " ";
-                    echo $value."-".$delimiter;
-                    break;
-                }
-            }*/
+			if($arr[$i] == $separator[$j])
+				$arr[$i] = ' ';
 		 
 	foreach($arr as $value)
 		$result = $result.$value;
 	return $result;
-    }
+}
 
 function MyExplAlt(array $separator, string $string)
 {
-	$arr = array();
 	$arr = str_split($string);
 	$arr_length = count($arr);
 	
 	$sep_length = count($separator);
 	
-	$isDoing = true;
+	$isDoing;
 	$sepPos;
 	
 	$result = '';
 	
-    echo $sep_length.' ';
 	if($separator == "" ||
 	   $separator == false ||
 	   $separator == null) 
@@ -55,15 +42,15 @@ function MyExplAlt(array $separator, string $string)
 	
 	for($i=0; $i<$sep_length; $i++)
 		do{
+			$isDoing = true;
 			$sepPos = array_search($separator[$i], $arr);
-		echo $sepPos.'<br />';
-		if($sepPos != false) 
+			if($sepPos != false) 
 				$arr[$sepPos] = ' ';
-		else
+			else
 				$isDoing = false;
 		}	
 		while($isDoing);
-	
+
 	foreach($arr as $value)
 		$result = $result.$value;
 	return $result;
@@ -71,6 +58,6 @@ function MyExplAlt(array $separator, string $string)
 
 $separatorArray = array(1, 2);
 $pizza = "pice1pice2pice1pice2pice1pice2pice1pice2pice1pice2pice1pice";
-$pieces = MyExplAlt($separatorArray, $pizza);                            
+$pieces = MyExpl($separatorArray, $pizza);                            
 
 echo $pieces;
